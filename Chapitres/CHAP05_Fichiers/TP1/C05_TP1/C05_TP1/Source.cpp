1b894be16b138e87e6e81e88c3a9713be5e42381
Vexliar <eloise.lecronier@gmail.com>
2022-12-02 15:26:21 +0100

    -> C05_TP1 création

---
+++ Chapitres/CHAP05_Fichiers/TP1/C05_TP1/C05_TP1/Source.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+
+
+// 0 / 4 Exercices 
+
+
+
+// ------- Exercice 1 ------- 
+
+	// Avec une application ecrite en C, vous allez ouvrir le fichier « source.txt » en lecture, et
+	// afficher son contenu caractere par caractere a l’ecran a l aide de la fonction getc.
+	// Refermez ensuite le fichier.
+
+int main(int argc, char ** argv)
+{
+	FILE * fichier;
+	fichier = fopen(", "r+");
+}
+
+// ------- Exercice 2 ------- 
+
+	// Avec une application ecrite en C, vous allez ouvrir le fichier « source.txt » en lecture, et
+	// afficher son contenu caractere par caractere à l ecran a l aide de la fonction getc.
+	// Refermez ensuite le fichier.
+
+
+// ------- Exercice 3 ------- 
+
+	// Ouvrir le fichier « source.txt » en lecture, et un fichier que vous nommerez « dest2.txt » en
+	// ecriture, puis vous copier chaque caractere du fichier source dans le fichier de destination
+	// mais sans copier la lettre ‘a’ des que vous la trouverez dans le fichier source.
+	// Refermez le fichier.
+
+
+// ------- Exercice 4 ------- 
+
+	// Ecrire une fonction ayant un pointeur de fichier en argument (pointeur sur un fichier qui 
+	// sera deja ouvert dans le programme principal) et qui retournera un entier. Cette fonction  
+	// aura pour but de calculer et de retourner la taille du fichier (en octets).
+	// Essayez votre fonction avec les trois fichiers utilises (source.txt, dest1.txt, dest2.txt).
+	// La fermeture du fichier se fera dans le programme principal.
+
